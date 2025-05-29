0a574e3dbaea2d11998ee3d8739fd4ca2f71a31f
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-28 21:56:00 -0300

    page route login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+export default function Login() {
+  return (
+    <div>
+      <header>
+        <h1>Login</h1>
+      </header>
+      <main>
+        Main
+      </main>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  );
+}
